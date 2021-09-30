e08231fb1e8cc8694988f1e7cf9e2951338471ac
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-30 02:08:51 -0500

    bar in progress

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Navbar from '../Navbar';
-
-
+import PropTypes from "prop-types";
+import NProgress from "nprogress";
+import nProgress from "nprogress";
+import classNames from "classnames";
 const Layout = ({children}) => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+        console.log(url);
+        NProgress.start();
+        };
+
+        router.events.on("routeChangeStart", handleRouteChange);
+
+        router.events.on("routeChangeComplete", () => NProgress.done());
+
+        router.events.on("routeChangeError", () => nProgress.done());
+
+        return () => {
+        router.events.off("routeChangeStart", handleRouteChange);
+        };
+    }, []);
+
     return (
         <>  
             
--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,10 @@ class MyDocument extends Document {
             <title>
                 My profolio
             </title>
+            <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+          />
         </Head>
         <body>
           <Main />
